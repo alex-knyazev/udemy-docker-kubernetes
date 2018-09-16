c3582fa378e92b1dcfe1b104def24feddba4a191
Alex Knyazev <alex.knyazev.world@gmail.com>
2018-09-17 00:26:16 +0300

    section 5 - introducing docker-compose

---
+++ README.md
@@ -0,0 +1,11 @@
+run docker compose:
+
+```
+docker-compose up
+```
+
+run docker-compose with rebuild:
+
+```
+docker-compose up --build
+```
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
 require('dotenv').config();
 
 const port = process.env.PORT;
 
 const app = express();
 
+const redisClient = redis.createClient({
+  // docker-compose understands host by name of service
+  host: 'redis-server',
+  port: 6379,
+});
+redisClient.set('visits', 0);
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hi there</h1>');
+  redisClient.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    redisClient.set('visits', parseInt(visits) + 1);
+  });
 });
 
 app.listen(port, () => {
